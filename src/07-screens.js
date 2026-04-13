4a1ec6c6d67b6404daf947605a9e421cb7ae606e
r0ze <r0ze998@users.noreply.github.com>
2026-04-14 06:54:40 +0900

    v110: collected cards orbit title screen as personalized showcase

--- src/07-screens.js
+++ src/07-screens.js
@@ -1172,7 +1172,7 @@ function dMap(){
     txShadow(wIcon,820,hudY+52,9,wCol,'rgba(0,0,0,.4)');
   }
   // Version label in HUD (bottom-right corner)
-  txShadow('v109',930,hudY+56,8,'#8890c0','rgba(0,0,0,.5)');
+  txShadow('v110',930,hudY+56,8,'#8890c0','rgba(0,0,0,.5)');
 
   // Day/night icon
   drawDayNightIcon(740,hudY+42);
